df571a37352d0a5bf24b8cc1958d989fd8d66c52
juanosarg <31105173+juanosarg@users.noreply.github.com>
2018-03-04 16:30:55 +0100

    Initial commit

---
+++ Source/GatesToTheUniverse/GatesToTheUniverse/Building_Farcaster.cs
@@ -0,0 +1,30 @@
+﻿
+using RimWorld;
+using Verse;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GatesToTheUniverse
+{
+    class Building_Farcaster : Building
+    {
+
+        public override IEnumerable<Gizmo> GetGizmos()
+        {
+           
+            Command_Action com = new Command_Action();
+            com.icon = ContentFinder<Texture2D>.Get("UI/Commands/Detonate", true);
+            com.defaultDesc = "CommandDetonateDesc".Translate();
+            //com.action = new Action(this.Command_Detonate);
+            if (base.GetComp<CompExplosive>().wickStarted)
+            {
+                com.Disable(null);
+            }
+            com.defaultLabel = "CommandDetonateLabel".Translate();
+            yield return com;
+        }
+
+    }
+}
